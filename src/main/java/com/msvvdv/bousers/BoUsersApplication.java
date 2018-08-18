5fef17298a62181685ac55f43f77d9ba192231a7
Felipe Sedevitiz <felipesedevitiz@gmail.com>
2018-08-18 16:29:10 -0300

    Initial Commit

---
+++ src/main/java/com/msvvdv/bousers/BoUsersApplication.java
@@ -0,0 +1,12 @@
+package com.msvvdv.bousers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoUsersApplication.class, args);
+	}
+}
---
+++ src/test/java/com/msvvdv/bousers/BoUsersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msvvdv.bousers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoUsersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
