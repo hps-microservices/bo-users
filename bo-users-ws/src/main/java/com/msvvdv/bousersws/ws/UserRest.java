ee944fce40a7a510c681fda2cb9d905a940541bd
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2018-09-03 14:28:08 -0300

    implements swagger

--- bo-users-ws/src/main/java/com/msvvdv/bousersws/ws/UserRest.java
+++ bo-users-ws/src/main/java/com/msvvdv/bousersws/ws/UserRest.java
@@ -1,14 +1,17 @@
 package com.msvvdv.bousersws.ws;
 
 import com.msvvdv.bouserscore.service.UserService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
+@Api(value = "Manage Users", tags = "User")
 public class UserRest {
 
-    @Autowired
-    private UserService service;
+    @GetMapping("/teste")
+    public void  test(){}
 }
